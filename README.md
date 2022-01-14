18df2c56eb608e94c64edc2380c1f6ee77cd8c6b
John Cardwell <cardwell.john@gmail.com>
2022-01-14 15:46:16 -0800

    reset yarn install stuff

---
+++ README.md
@@ -0,0 +1,1 @@
+# brcc-workspaces
---
+++ packages/brcc-headless-build/next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  reactStrictMode: true,
+  experimental: {
+    // this will allow nextjs to resolve files (js, ts, css)
+    // outside packages/app directory. 
+    externalDir: true,
+  },
+}
